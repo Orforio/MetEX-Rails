f56624f63ad0ff7f95f23646f841f167aeefe33f
Richard Whittaker <pkeruno@gmail.com>
2015-09-03 23:57:11 +0100

    Add indexes to Movement migration

--- db/migrate/20150903222733_create_movements.rb
+++ db/migrate/20150903222733_create_movements.rb
@@ -1,8 +1,8 @@
 class CreateMovements < ActiveRecord::Migration
   def change
     create_table :movements do |t|
-      t.integer :up_station_id
-      t.integer :down_station_id
+      t.integer :up_station_id, index: true
+      t.integer :down_station_id, index: true
       t.boolean :up_allowed
       t.boolean :down_allowed
       t.integer :length
