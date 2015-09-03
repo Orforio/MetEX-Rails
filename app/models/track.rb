7da26859e0117678515cc45b78b8d94b9bcee60e
Richard Whittaker <pkeruno@gmail.com>
2015-09-04 00:09:12 +0100

    Remove Track model - HABTM is more appropriate

--- app/models/track.rb
+++
@@ -1,4 +0,0 @@
-class Track < ActiveRecord::Base
-  belongs_to :station_id
-  belongs_to :movement_id
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903224156) do
+ActiveRecord::Schema.define(version: 20150903222733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,16 +53,4 @@ ActiveRecord::Schema.define(version: 20150903224156) do
   add_index "stations", ["line_id"], name: "index_stations_on_line_id", using: :btree
   add_index "stations", ["slug", "line_id"], name: "index_stations_on_slug_and_line_id", unique: true, using: :btree
 
-  create_table "tracks", force: :cascade do |t|
-    t.integer  "station_id"
-    t.integer  "movement_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "tracks", ["movement_id"], name: "index_tracks_on_movement_id", using: :btree
-  add_index "tracks", ["station_id"], name: "index_tracks_on_station_id", using: :btree
-
-  add_foreign_key "tracks", "movements"
-  add_foreign_key "tracks", "stations"
 end
