0f077ac54c7d87c926e923cf577e195b8566b6fa
Richard Whittaker <pkeruno@gmail.com>
2015-09-06 18:45:48 +0100

    Change Station sequential navigation to handle terminus stations

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -1,2 +1,9 @@
 module StationsHelper
+	def link_to_next_station(station)
+		if station
+			link_to station.name, line_station_path(station.line.slug, station.slug)
+		else
+			"Terminus"
+		end
+	end
 end
