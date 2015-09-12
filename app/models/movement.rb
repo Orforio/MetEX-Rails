d973bd3c37c106dba0fc7bd8388a245f0a2cc35b
Richard Whittaker <pkeruno@gmail.com>
2015-09-12 23:56:25 +0100

    Add fixtures and unit tests for Movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -2,4 +2,14 @@ class Movement < ActiveRecord::Base
 	belongs_to :up_station, class_name: 'Station', inverse_of: :down_movements
 	belongs_to :down_station, class_name: 'Station', inverse_of: :up_movements
 	has_and_belongs_to_many :stations
+	
+	validates :stations, presence: true
+	validate :both_stations_cannot_be_nil
+	
+	private
+		def both_stations_cannot_be_nil
+			unless up_station_id.present? || down_station_id.present?
+				errors.add(:base, "both stations can't be empty")
+			end
+		end
 end
--- test/models/movement_test.rb
+++ test/models/movement_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class MovementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	should belong_to(:up_station)
+	should belong_to(:down_station)
+	should have_and_belong_to_many(:stations)
+	
+	should validate_presence_of(:stations)
+	
+	context "Movement" do
+		setup do
+			@movement1 = movements(:movement1)
+		end
+		
+		should "not validate with both stations nil" do
+			@movement1.up_station = nil
+			@movement1.down_station = nil
+			@movement1.valid?
+			assert_match /both stations can't be empty/, @movement1.errors[:base].join
+		end
+		
+		should "validate with either station nil" do
+			@movement1.up_station = nil
+			assert @movement1.valid?
+		end
+		
+		should "validate with both stations present" do
+			assert @movement1.valid?
+		end
+	end
 end
