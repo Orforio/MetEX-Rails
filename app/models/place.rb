a9b24b536af5111164a0950d3ce796121a777f75
Richard Whittaker <pkeruno@gmail.com>
2015-09-12 19:02:42 +0100

    Add fixtures and unit tests for Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
 	has_many :images, as: :imageable
 	has_and_belongs_to_many :stations
+	
+	validates :name, :description, :stations, presence: true
+	validates :name, uniqueness: true
 end
--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	should have_many(:images)
+	should have_and_belong_to_many(:stations)
+	
+	should validate_presence_of(:name)
+	should validate_presence_of(:description)
+	should validate_presence_of(:stations)
+	should validate_uniqueness_of(:name)
 end
