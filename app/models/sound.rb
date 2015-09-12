4f1ec7a9debd0953a29d4b6c873d78f23056ae96
Richard Whittaker <pkeruno@gmail.com>
2015-09-12 18:26:19 +0100

    Add fixtures and unit tests for Sound Model

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,3 +1,11 @@
 class Sound < ActiveRecord::Base
 	belongs_to :soundable, polymorphic: true
+	
+	validates :filename, :title, :length, :soundable, presence: true
+	validates :filename, uniqueness: true, format: { with: /[\w\/]+\.mp3\z/, message: "must end in .mp3" }
+	validates :length, numericality: { only_integer: true }
+	
+	def url
+		"http://metexv2.sblorgh.org/media/audio/#{filename}"
+	end
 end
--- test/models/sound_test.rb
+++ test/models/sound_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SoundTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	should belong_to(:soundable)
+	
+	should validate_presence_of(:filename)
+	should validate_presence_of(:title)
+	should validate_presence_of(:length)
+	should validate_presence_of(:soundable)
+	should validate_uniqueness_of(:filename)
+	should allow_value("filename.mp3").for(:filename)
+	should_not allow_value("filename").for(:filename)
+	should validate_numericality_of(:length)
+	
+	context "filename_url" do
+		setup do
+			@sound1 = sounds(:sound1)
+		end
+		
+		should "return a fully-formed URL" do
+			assert_equal "http://metexv2.sblorgh.org/media/audio/1/ambience.mp3", @sound1.url
+		end
+	end
 end
