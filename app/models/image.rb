b9bed9d9ccabcbc9af049a97155577ab4bc45ceb
Richard Whittaker <pkeruno@gmail.com>
2015-09-13 02:26:41 +0100

    Add fixtures and unit tests for Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,10 @@
 class Image < ActiveRecord::Base
 	belongs_to :imageable, polymorphic: true
+	
+	validates :filename, :title, :alt, :imageable, presence: true
+	validates :filename, format: { with: /[\w\/-]+\.jpg\z/, message: "must end in .jpg" }
+	
+	def url
+		"http://metexv2.sblorgh.org/media/images/stations/#{filename}"
+	end
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	should belong_to(:imageable)
+	
+	should validate_presence_of(:filename)
+	should validate_presence_of(:title)
+	should validate_presence_of(:alt)
+	should validate_presence_of(:imageable)
+	should allow_value("filename.jpg").for(:filename)
+	should_not allow_value("filename").for(:filename)
+	
+	context "image.url" do
+		setup do
+			@image1 = images(:image1)
+		end
+		
+		should "return a fully-formed URL" do
+			assert_equal "http://metexv2.sblorgh.org/media/images/stations/14/123-2.jpg", @image1.url
+		end
+	end
 end
